3c03ef90572dd50ca08d827e35786158093aeea6
Jan Dočkal <dockal07@gmail.com>
2019-03-23 06:26:01 +1300

    Form is handled properly when switching from edit to new

--- src/Components/EmployeeForm/EmployeeForm.tsx
+++ src/Components/EmployeeForm/EmployeeForm.tsx
@@ -8,21 +8,36 @@ import { StyledFormGroup } from '../Styled/Form/StyledFormGroup';
 import { StyledFormInput } from '../Styled/Form/StyledFormInput';
 import { StyledFormSelect } from '../Styled/Form/StyledFormSelect';
 
-type IEmployeeForm = Partial<IEmployee>;
-
 interface IProps {
 	editEmployee?: IEmployee;
 	onSubmit(employee: IEmployee): void;
 }
 
 interface IState {
-	employee: IEmployeeForm;
+	employee: IEmployee;
+}
+
+const employeeDefaults: IEmployee = {
+	address: '',
+	email: '',
+	img: '',
+	name: '',
+	role: '' as any,
+	team: '' as any,
 }
 
 export class EmployeeForm extends React.PureComponent<IProps, IState> {
 	public state: IState = {
-		employee: this.props.editEmployee ? this.props.editEmployee : {},
+		employee: this.props.editEmployee ? this.props.editEmployee : employeeDefaults,
 	};
+
+	public componentDidUpdate(prevProps: IProps) {
+		if (prevProps.editEmployee && prevProps.editEmployee.email && !this.props.editEmployee) {
+			this.setState({
+				employee: employeeDefaults,
+			});
+		}
+	}
 
 	public render() {
 		const editing = !!this.props.editEmployee;
@@ -33,19 +48,19 @@ export class EmployeeForm extends React.PureComponent<IProps, IState> {
 						<div>
 							<label htmlFor='name'>Name</label>
 						</div>
-						<StyledFormInput onChange={this.handleInputChange} id='name' name='name' type='text' defaultValue='' value={this.state.employee.name} required={true} />
+						<StyledFormInput onChange={this.handleInputChange} id='name' name='name' type='text' value={this.state.employee.name} required={true} />
 					</StyledFormGroup>
 					<StyledFormGroup>
 						<div>
 							<label htmlFor='email'>Email address</label>
 						</div>
-						<StyledFormInput onChange={this.handleInputChange} id='email' name='email' type='email' disabled={editing} defaultValue='' value={this.state.employee.email} required={true} />
+						<StyledFormInput onChange={this.handleInputChange} id='email' name='email' type='email' disabled={editing} value={this.state.employee.email} required={true} />
 					</StyledFormGroup>
 					<StyledFormGroup>
 						<div>
 							<label htmlFor='role'>Role</label>
 						</div>
-						<StyledFormSelect onChange={this.handleInputChange} id='role' name='role' value={this.state.employee.role} defaultValue='' required={true}>
+						<StyledFormSelect onChange={this.handleInputChange} id='role' name='role' value={this.state.employee.role} required={true}>
 							<option value={''} defaultChecked={true} disabled={true}>Plase select a role</option>
 							{
 								Object.keys(IRole).map((role: string) =>
@@ -58,7 +73,7 @@ export class EmployeeForm extends React.PureComponent<IProps, IState> {
 						<div>
 							<label htmlFor='team'>Team</label>
 						</div>
-						<StyledFormSelect onChange={this.handleInputChange} id='team' name='team' value={this.state.employee.team} defaultValue='' required={true} >
+						<StyledFormSelect onChange={this.handleInputChange} id='team' name='team' value={this.state.employee.team} required={true} >
 							<option value='' disabled={true}>Plase select a team</option>
 							{
 								Object.keys(ITeam).map((team: string) =>
@@ -71,7 +86,7 @@ export class EmployeeForm extends React.PureComponent<IProps, IState> {
 						<div>
 							<label htmlFor='address'>Address</label>
 						</div>
-						<StyledFormInput onChange={this.handleInputChange} id='address' name='address' type='text' defaultValue='' value={this.state.employee.address} required={true} />
+						<StyledFormInput onChange={this.handleInputChange} id='address' name='address' type='text' value={this.state.employee.address} required={true} />
 					</StyledFormGroup>
 					<StyledButton scheme={StyledButtonScheme.GREEN}>
 						Submit
