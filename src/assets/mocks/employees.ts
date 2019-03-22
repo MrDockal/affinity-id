e66012baf5d5f3ab86b41c8b4a8ff51100eb0bd3
Jan Dočkal <dockal07@gmail.com>
2019-03-22 17:30:28 +1300

    Prepare employee mock data

---
+++ src/assets/mocks/employees.ts
@@ -0,0 +1,51 @@
+import { IEmployee } from 'src/Model/Employee/IEmployee';
+import jackImg from '../img/jack.png';
+import victoriaImg from '../img/victoria.png';
+import donnaImg from '../img/donna.png';
+import marryImg from '../img/marry.png';
+import andyImg from '../img/andy.png';
+import { IRole } from 'src/Model/Employee/IRole';
+import { ITeam } from 'src/Model/Team/ITeam';
+
+export const employees: IEmployee[] = [
+	{
+		img: jackImg,
+		name: 'Jack Jackson',
+		address: '307 Brand road, Auckland',
+		email: 'jack@affinityid.co.nz',
+		role: IRole.Admin,
+		team: ITeam.Creative,
+	},
+	{
+		img: victoriaImg,
+		name: 'Victoria Furgason',
+		address: '308 Brand road, Auckland',
+		email: 'victoria@affinityid.co.nz',
+		role: IRole.Admin,
+		team: ITeam.Management,
+	},
+	{
+		img: donnaImg,
+		name: 'Donna Hicks',
+		address: '309 Brand road, Auckland',
+		email: 'Donna@affinityid.co.nz',
+		role: IRole.Admin,
+		team: ITeam.AdminFinance,
+	},
+	{
+		img: marryImg,
+		name: 'Marry Long',
+		address: '310 Brand road, Auckland',
+		email: 'andy@affinityid.co.nz',
+		role: IRole.Employee,
+		team: ITeam.Creative,
+	},
+	{
+		img: andyImg,
+		name: 'Andy Mann',
+		address: '311 Brand road, Auckland',
+		email: 'andy@affinityid.co.nz',
+		role: IRole.Employee,
+		team: ITeam.Creative,
+	}
+];
